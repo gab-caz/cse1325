327622e4434647e26aad0dc2be9ecaa7529a3549
Gabriela Cazares <gxc7200@mavs.uta.edu>
2024-09-01 15:40:16 -0500

    P02 Full Credit

--- P02/full_credit/Purse.java
+++ P02/full_credit/Purse.java
@@ -31,7 +31,7 @@ public class Purse
             }
         }
 
-        System.out.printf("\nYou have a total of $%.2f%n" + total);
+        System.out.printf(String.format("\nYou have a total of $%.2f%n" + total));
         System.out.printf("\nThe coins span the years " + earliestDate + "-" + latestDate);
     }
 }
