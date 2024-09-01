7df71a5a70c6d8bc296593ab85bb006c6be89b8a
Gabriela Cazares <gxc7200@mavs.uta.edu>
2024-09-01 14:31:39 -0500

    P02 Full Credit

--- P02/full_credit/Purse.java
+++ P02/full_credit/Purse.java
@@ -1,36 +1,36 @@
 public class Purse
 {
-	public static void main(String[] args)
-	{
-		Coin[8] Money = { new Coin(Denomination.PENNY, 1904),
-						  new Coin(Denomination.NICKEL, 1945),
-						  new Coin(Denomination.DIME, 1996),
-						  new Coin(Denomination.QUARTER, 2006),
-						  new Coin(Denomination.PENNY, 1975),
-						  new Coin(Denomination.NICKEL, 1981),
-						  new Coin(Denomination.DIME, 1998),
-						  new Coin(Denomination.QUARTER, 2003) };
-		
-		double total = 0;
-		int earliestDate = Money[0].getYear();
-		int latestDate = Money[0].getYear();
+    public static void main(String[] args)
+    {
+        Coin[8] Money = { new Coin(Denomination.PENNY, 1904),
+                          new Coin(Denomination.NICKEL, 1945),
+                          new Coin(Denomination.DIME, 1996),
+                          new Coin(Denomination.QUARTER, 2006),
+                          new Coin(Denomination.PENNY, 1975),
+                          new Coin(Denomination.NICKEL, 1981),
+                          new Coin(Denomination.DIME, 1998),
+                          new Coin(Denomination.QUARTER, 2003) };
+        
+        double total = 0;
+        int earliestDate = Money[0].getYear();
+        int latestDate = Money[0].getYear();
 
-		for (Coin coin : Money)
-		{
-			total += coin.getValue();
-			
-			if (coin.getYear() < earliestDate)
-			{
-				earliestDate = coin.getYear();
-			}
-			
-			if (coin.getYear() > latestDate)
-			{
-				latestDate = coin.getYear();
-			}
-		}
+        for (Coin coin : Money)
+        {
+            total += coin.getValue();
+            
+            if (coin.getYear() < earliestDate)
+            {
+                earliestDate = coin.getYear();
+            }
+            
+            if (coin.getYear() > latestDate)
+            {
+                latestDate = coin.getYear();
+            }
+        }
 
-		System.out.println("Your wallet has $" + total);
-		System.out.println("The coins span the years " + earliestDate + "-" + latestDate);
-	}
+        System.out.println("Your wallet has $" + total);
+        System.out.println("The coins span the years " + earliestDate + "-" + latestDate);
+    }
 }
