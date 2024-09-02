060575005df964fb966c22f12c5ecea42de6e32c
Gabriela Cazares <gxc7200@mavs.uta.edu>
2024-09-02 14:37:02 -0500

    P02 Extreme Bonus

---
+++ P02/extreme_bonus/Coin.java
@@ -0,0 +1,32 @@
+public class Coin
+{
+    private Denomination denomination;
+    private int year;
+    
+    public Coin(Denomination denomination, int year)
+    {
+        this.denomination = denomination;
+        this.year = year;
+    }
+
+    public double getValue()
+    {
+        return denomination.getValue();
+    }
+
+    public double getWeight()
+    {
+        
+    }
+    
+    public int getYear()
+    {
+        return year;
+    }
+
+    @Override
+    public String toString()
+    {
+        return year + " " + denomination.toString();
+    }
+}
