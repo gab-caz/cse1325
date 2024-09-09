727802c85f539a907cb4033a52863e07394177c5
Gabriela Cazares <gxc7200@mavs.uta.edu>
2024-09-08 20:44:43 -0500

    Added onto the 3 tests...need to finish

--- P03/TestStudent.java
+++ P03/TestStudent.java
@@ -2,38 +2,49 @@ public class TestStudent
 {
     public static void main(String[] args)
     {
-        try//Test1:verify toString returns correct representation 
-        {
-            String expected = "Name(id,email,account number)";
+        int failureCount = 0;
 
+        //Test1:verify toString returns correct representation
+        String expectedtoString = "name(id,email,account#)";
+        Student student = new Student();
+        if (!expectedtoString.equals(student.toString()))
+        {
+            System.err.println("Test 1 Failed. toString() not correct representation.");
+            failureCount++;
+        }
+
+
+        //Test2:verify that if nonUTA email is used, throw error(3 failure)
+        try
+        {
+            new Student();
+            System.err.println("Test 2 Failed. ");
+            failureCount++;
+        }
+        catch (IllegalArgumentException e)
+        {
+            System.err.println("Test 2 Failed. ");
+            failureCount++;
         }
         catch (Exception e)
         {
-            System.err.println("Test 1 Failed.");
-
+            System.err.println("Test 2 Failed ");
+            failureCount++;
         }
 
 
-        try//Test2:verify that if nonUTA email is used, throw error(3 failure)
+        //test3:verify media returns "Playing" and result
+        try
         {
-
+            System.err.println("Test 3 Failed. ");
+            failureCount++;
         }
         catch (Exception e)
         {
-            System.err.println("Test 2 Failed.");
-            
+            System.err.println("Test 3 Failed. ");
+            failureCount++;         
         }
 
-
-        try//test3:verify media returns "Playing" and result
-        {
-            String expected = "Playing " + media.toString();
-
-        }
-        catch (Exception e)
-        {
-            System.err.println("Test 3 Failed.");
-            
-        }
+        System.exit(failureCount);
     }
 }
