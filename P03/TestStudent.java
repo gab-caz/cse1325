a4931306cd83a1ad58b2e47fe7329e7a3e3986de
Gabriela Cazares <gxc7200@mavs.uta.edu>
2024-09-08 20:28:34 -0500

    Adding onto Bonus...need to finish

--- P03/TestStudent.java
+++ P03/TestStudent.java
@@ -2,6 +2,38 @@ public class TestStudent
 {
     public static void main(String[] args)
     {
-        
+        try//Test1:verify toString returns correct representation 
+        {
+            String expected = "Name(id,email,account number)";
+
+        }
+        catch (Exception e)
+        {
+            System.err.println("Test 1 Failed.");
+
+        }
+
+
+        try//Test2:verify that if nonUTA email is used, throw error(3 failure)
+        {
+
+        }
+        catch (Exception e)
+        {
+            System.err.println("Test 2 Failed.");
+            
+        }
+
+
+        try//test3:verify media returns "Playing" and result
+        {
+            String expected = "Playing " + media.toString();
+
+        }
+        catch (Exception e)
+        {
+            System.err.println("Test 3 Failed.");
+            
+        }
     }
 }
