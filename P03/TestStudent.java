46eb50c77a2d324d44232f46539c83c778588cfb
Gabriela Cazares <gxc7200@mavs.uta.edu>
2024-09-09 13:43:58 -0500

    Test3 should be good now

--- P03/TestStudent.java
+++ P03/TestStudent.java
@@ -6,7 +6,7 @@ public class TestStudent
 
         //Test1:verify toString returns correct representation
         String expectedtoString = "Jane Doe (1234567890, jxd@mavs.uta.edu, Account #1)";
-        Student student = new Student("Jane Doe", 1234567890, "jxd@mavs.uta.edu");
+        Student studentTest1 = new Student("Jane Doe", 1234567890, "jxd@mavs.uta.edu");
         if (!expectedtoString.equals(student.toString()))
         {
             System.err.println("Test 1 Failed...toString() method did not return correct representation.");
@@ -17,7 +17,7 @@ public class TestStudent
         //Test2:verify that if nonUTA email is used, throw error(3 failure)
         try
         {
-            new Student("John Doe", 9087654321, "jd@gmail.com");
+            Student studentTest2 = new Student("John Doe", 9087654321, "jd@gmail.com");
             System.err.println("Test 2 Failed...");
             failureCount++;
         }
@@ -34,11 +34,13 @@ public class TestStudent
 
 
         //Test3:verify media returns "Playing" and media.toString() result
+        Student studentTest3 = new Student ("John Smith", 1234567098, "jxs@uta.edu");
         String expectedMedia = "Playing Title (https://url.com)";
         Media media = new Media("Title", "https://url.com");
-        if (!(expectedMedia))
+        String mediaResult = studentTest3.requestMedia(media);
+        if (!mediaResult.equals(expectedMedia))
         {
-            System.err.println("Test 3 Failed...requesting media returned unexpected results");
+            System.err.println("Test 3 Failed...requesting media returned unexpected results. " + mediaResult);
             failureCount++;
         }
         
