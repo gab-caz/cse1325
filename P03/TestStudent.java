f58a22ce5dafe8376dd63e74d67f87257e7f9f67
Gabriela Cazares <gxc7200@mavs.uta.edu>
2024-09-09 17:02:35 -0500

    actually going to variables so added that at top

--- P03/TestStudent.java
+++ P03/TestStudent.java
@@ -6,9 +6,25 @@ public class TestStudent
     {
         int failureCount = 0;
 
+        //Test1
+        String name1 = "Jane Doe";
+        int id1 = 1001762359;
+        String email1 = "jxd@mavs.uta.edu";
+
+        //Test2
+        String name2 = "John Doe";
+        int id2 = 1001369223;
+        String email2 = "john.doe@gmail.com";
+
+        //Test3
+        String name3 = "John Smith";
+        int id3 = 1001772245;
+        String email3 = "jxs@uta.edu";
+
+
         //Test1:verify toString returns correct representation
-        Student studentTest1 = new Student("Jane Doe", 1001234567, "jxd@mavs.uta.edu");
-        String expectedtoString = "Jane Doe (1234567890, jxd@mavs.uta.edu, Account #" + studentTest1.getAccount().getAccountNumber() + ")";
+        Student studentTest1 = new Student(name1, id1, email1);
+        String expectedtoString = name1 + " (" + id1 + ", " + email1 + ", Account #" + studentTest1.getAccount().getAccountNumber() + ")";
         if (!studentTest1.toString().equals(expectedtoString))
         {
             System.err.println("Test 1 Failed...toString() method did not return correct representation: " + studentTest1.toString());
@@ -19,7 +35,7 @@ public class TestStudent
         //Test2:verify that if nonUTA email is used, throw error(3 failure)
         try
         {
-            Student studentTest2 = new Student("John Doe", 10019876543, "john.doe@gmail.com");
+            Student studentTest2 = new Student(name2, id2, email2);
             System.err.println("Test 2 Failed...IllegalArgumentException was not thrown for incorrect email.");
             failureCount++;
         }
@@ -39,7 +55,7 @@ public class TestStudent
 
 
         //Test3:verify media returns "Playing" and media.toString() result
-        Student studentTest3 = new Student ("John Smith", 10016543298, "jxs@uta.edu");
+        Student studentTest3 = new Student (name3, id3, email3);
         String expectedMedia = "Playing Title (https://url.com)";
         Media media = new Media("Title", "https://url.com");
         String mediaResult = studentTest3.requestMedia(media);
