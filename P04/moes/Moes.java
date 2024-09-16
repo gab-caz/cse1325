0dd9605b956141fe6bca24b2eb9218b6d5dbf97a
Gabriela Cazares <gxc7200@mavs.uta.edu>
2024-09-15 21:00:32 -0500

    added rest of the methods and fields

--- P04/moes/Moes.java
+++ P04/moes/Moes.java
@@ -9,5 +9,41 @@ import java.util.ArrayList;
 
 public class Moes
 {
+    private ArrayList<Media> library = new ArrayList<>();
+    private ArrayList<Student> customers = new ArrayList<>();
 
+    public void addMedia(Media media)
+    {
+        library.add(media);
+    }
+
+    public String getMediaList()
+    {
+
+    }
+
+    public addStudent(Student student)
+    {
+
+    }
+
+    public String getStudentList()
+    {
+
+    }
+
+    public int getPoints(studentIndex)
+    {
+
+    }
+
+    public String buyPoints(int studentIndex, int points)
+    {
+
+    }
+
+    public String playMedia(int studentIndex, int mediaIndex)
+    {
+
+    }
 }
