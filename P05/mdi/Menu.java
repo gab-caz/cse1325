965c8626c4f0ffc8244a3ce1f9a09781fbd6e409
Gabriela Cazares <gxc7200@mavs.uta.edu>
2024-09-28 17:22:59 -0500

    added the getString and getInt

--- P05/mdi/Menu.java
+++ P05/mdi/Menu.java
@@ -1,28 +1,90 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Menu
 {
-	private ArrayList<MenuItem> items = new ArrayList<>();
+    private ArrayList<MenuItem> items = new ArrayList<>();
+    private static Scanner in = new Scanner(System.in);
 
-	public void addMenuItem(MenuItem item)
-	{
-		items.add(item);
-	}
+    public void addMenuItem(MenuItem item)
+    {
+        items.add(item);
+    }
 
-	@Override
-	public String toString()
-	{
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < items.size(); ++i)
-		{
-			sb.append(" " + i + "] " + items.get(i) + "\n");
-		}
-		return sb.toString();
-	}
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < items.size(); ++i)
+        {
+            sb.append(" " + i + "] " + items.get(i) + "\n");
+        }
+        return sb.toString();
+    }
 
-	public void run(int itemNumber)
-	{
-		items.get(itemNumber).run();
-	}
+    public void run(int itemNumber)
+    {
+        items.get(itemNumber).run();
+    }
+
+    public static String getString(String prompt, String cancelInput, String defaultInput)
+    {
+        String s = null;
+        while(true)
+        {
+            try
+            {
+                System.out.print(prompt);
+                s = in.nextLine().trim();
+                if(s.isEmpty() && defaultInput != null)
+                {
+                    s = defaultInput;
+                }
+                break;
+            }
+            catch(Exception e)
+            {
+                System.err.println("Invalid input! Try again.");
+            }
+        }
+        
+        if(cancelInput != null && s.equals(cancelInput))
+        {
+            s = null; 
+        }
+        return s;
+    }
+
+    public static String getString(String prompt, String cancelInput)
+    {
+        return getString(prompt, cancelInput, null);
+    }
+
+    public static Integer getInt(String prompt, String cancelInput, String defaultInput)
+    {
+        Integer i = null;
+        while(true)
+        {
+            try 
+            {
+                String s = getString(prompt, cancelInput, defaultInput);
+                if(s != null && !s.isEmpty())
+                {
+                    i = Integer.parseInt(s);
+                }
+                break;
+            }
+            catch(Exception e)
+            {
+                System.err.println("Invalid input! Try again.");
+            }
+        }
+        return i;
+    }
+
+    public static Integer getInt(String prompt, String cancelInput)
+    {
+        return getInt(prompt, cancelInput, null);
+    }
 }
--- P05/mdi/MenuItem.java
+++ P05/mdi/MenuItem.java
@@ -1,23 +1,23 @@
 public class MenuItem implements Runnable
 {
-	private Object menuText;
-	private Runnable menuResponse;
+    private Object menuText;
+    private Runnable menuResponse;
 
-	public MenuItem(Object menuText, Runnable menuResponse)
-	{
-		this.menuText = menuText;
-		this.menuResponse = menuResponse;
-	}
+    public MenuItem(Object menuText, Runnable menuResponse)
+    {
+        this.menuText = menuText;
+        this.menuResponse = menuResponse;
+    }
 
-	@Overrride
-	public String toString()
-	{
-		return menuText.toString();
-	}
+    @Overrride
+    public String toString()
+    {
+        return menuText.toString();
+    }
 
-	@Overrride
-	public void run()
-	{
-		menuResponse.run();
-	}
+    @Overrride
+    public void run()
+    {
+        menuResponse.run();
+    }
 }
