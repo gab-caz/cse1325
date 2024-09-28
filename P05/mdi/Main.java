36412b99d7a148c4759686bade1e7bf8647ae0c3
Gabriela Cazares <gxc7200@mavs.uta.edu>
2024-09-28 18:06:51 -0500

    getting started on my main

---
+++ P05/mdi/Main.java
@@ -0,0 +1,57 @@
+public class Main
+{
+	private Moes moes;
+	private String output;
+	private Menu menu;
+	private boolean running;
+
+	private addStudent()
+	{
+
+	}
+
+	private listStudents()
+	{
+
+	}
+
+	private addMedia()
+	{
+
+	}
+
+	private playMedia()
+	{
+
+	}
+
+	private listMedia()
+	{
+
+	}
+
+	private listAvailablePoints()
+	{
+
+	}
+
+	private buyPoints()
+	{
+
+	}
+
+	public void static main(String[] args)
+	{
+
+	}
+
+	private mdi()
+	{
+
+	}
+
+	private endApp()
+	{
+
+	}
+}
