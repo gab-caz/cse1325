5d2f341c75fbb9fb048ffe22651d116223474c57
Gabriela Cazares <cazgab67@gmail.com>
2024-10-24 19:37:14 -0500

    working on figuring out the vectors and arguments format from class notes

--- P08/bonus/nums.cpp
+++ P08/bonus/nums.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Numbers: ";
-    
+    try
+    {
+        std::vector<std::string> numbers;
+        std::vector<std::string> words;
+
+        for(int i = 1; i < argc; i++)
+        {
+
+        }
+
+        std::cout << "Numbers: " << std::endl;
+        std::cout << "Words: " << std::endl;
+    }
+    catch(...)
+    {
+        
+    }
+
 }
