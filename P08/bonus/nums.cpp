b6c7799386e481ba1c08f4f2f2589f7253b5ba22
Gabriela Cazares <cazgab67@gmail.com>
2024-10-25 12:35:08 -0500

    actually don't think will need the trycatch

--- P08/bonus/nums.cpp
+++ P08/bonus/nums.cpp
@@ -5,43 +5,36 @@
 
 int main(int argc, char* argv[])
 {
-    try
+    std::vector<std::string> numbers;
+    std::vector<std::string> words;
+
+    int index = 0;
+
+    while(index+1 < argc)
     {
-        std::vector<std::string> numbers;
-        std::vector<std::string> words;
+        std::string arg = argv[index+1];
 
-        int index = 0;
-
-        while(index+1 < argc)
+        if(std::isdigit(arg[0]))
         {
-            std::string arg = argv[index+1];
-
-            if(std::isdigit(arg[0]))
-            {
-                numbers.push_back(arg);
-            }
-            else
-            {
-                words.push_back(arg);
-            }
-
-            ++index;
+            numbers.push_back(arg);
+        }
+        else
+        {
+            words.push_back(arg);
         }
 
-        std::cout << "Numbers: " << std::endl;
-        for(size_t i = 0; i < numbers.size(); ++i)
-        {
-            std::cout << numbers[i] << std::endl;
-        }
-
-        std::cout << "\nWords: " << std::endl;
-        for(size_t i = 0; i < words.size(); ++i)
-        {
-            std::cout << words[i] << std::endl;
-        }
+        ++index;
     }
-    catch(...)
+
+    std::cout << "Numbers: " << std::endl;
+    for(size_t i = 0; i < numbers.size(); ++i)
     {
-        std::cerr << "Error, expected format: ./nums 3 plus 2 is 5. Received: " << argv[0] << std::endl;
+        std::cout << numbers[i] << std::endl;
     }
+
+    std::cout << "\nWords: " << std::endl;
+    for(size_t i = 0; i < words.size(); ++i)
+    {
+        std::cout << words[i] << std::endl;
+    }    
 }
