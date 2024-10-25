802c7b2f1a75a919ad9d31c84622fcdbddf2aa94
Gabriela Cazares <cazgab67@gmail.com>
2024-10-24 20:36:29 -0500

    copying my bonus solution to extreme bonus

---
+++ P08/extreme_bonus/nums.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        std::vector<std::string> numbers;
+        std::vector<std::string> words;
+
+        int index = 0;
+
+        while(index+1 < argc)
+        {
+            std::string arg = argv[index+1];
+
+            if(std::isdigit(arg[0]))
+            {
+                numbers.push_back(arg);
+            }
+            else
+            {
+                words.push_back(arg);
+            }
+
+            ++index;
+        }
+
+        std::cout << "Numbers: " << std::endl;
+        for(size_t i = 0; i < numbers.size(); ++i)
+        {
+            std::cout << numbers[i] << std::endl;
+        }
+
+        std::cout << "\nWords: " << std::endl;
+        for(size_t i = 0; i < words.size(); ++i)
+        {
+            std::cout << words[i] << std::endl;
+        }
+    }
+    catch(...)
+    {
+        std::cerr << "Error, expected format: ./nums 3 plus 2 is 5. Received: " << argv[0] << std::endl;
+    }
+}
