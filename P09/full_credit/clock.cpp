296341c8bed2f36ece8959edec1692f284541b3f
Gabriela Cazares <cazgab67@gmail.com>
2024-11-01 15:28:33 -0500

    adding to my cpp file, first with the constructor

--- P09/full_credit/clock.cpp
+++ P09/full_credit/clock.cpp
@@ -1,14 +1,34 @@
-class Clock
+#include "clock.h"
+
+Clock::Clock(std::int hours, int minutes, int seconds) : _hours{hours}, _minutes{minutes}, _seconds{seconds}
 {
-	public:
-        Clock(std::int hours, int minutes, int seconds)
-        : _hours{hours}, _minutes{minutes}, _seconds{seconds}
-        {
+    if(hours < 0 || hours > 23)
+    {
+        throw std::out_of_range("Hours have to be between 0-23");
+    }
+    
+    if(minutes < 0 || minutes > 59)
+    {
+        throw std::out_of_range("Minutes have to be between 0-59");
+    }
+    
+    if(seconds < 0 || seconds > 59)
+    {
+        throw std::out_of_range("Seconds have to be between 0-59");
+    }
+}
 
-        }
+virtual ~Clock()//empty
+{
 
-	protected:
-        std::int _hours;
-        std::int _minutes;
-        std::int _seconds;
+}
+
+void Clock::tic()
+{
+
+}
+
+void print()
+{
+
 }
--- P09/full_credit/clock.h
+++ P09/full_credit/clock.h
@@ -5,16 +5,16 @@
 
 class Clock
 {
-	public:
-		Clock(int hours, int minutes, int seconds);
-		virtual ~Clock() {}
-		void tic();
-		void print();
+    public:
+        Clock(int hours, int minutes, int seconds);
+        virtual ~Clock() {}
+        void tic();
+        void print();
 
-	protected:
-		int _hours;
-		int _minutes;
-		int _seconds;
+    protected:
+        int _hours;
+        int _minutes;
+        int _seconds;
 };
 
 #endif
