6918ec240207442316b644397eb88efda1c066c8
Gabriela Cazares <cazgab67@gmail.com>
2024-11-03 18:27:10 -0600

    finished the timer files

--- P09/bonus/timer.cpp
+++ P09/bonus/timer.cpp
@@ -1,8 +1,9 @@
+#include <stdexcept>
 #include "timer.h"
 
-Timer::Timer(int hours, int minutes, int seconds) : _hours{hours}, _minutes{minutes}, _seconds{seconds}
+Timer::Timer(int hours, int minutes, int seconds) : Clock(hours, minutes, seconds)
 {
-
+//no body
 }
 
 Timer::~Timer()//empty
@@ -12,5 +13,24 @@ Timer::~Timer()//empty
 
 void Timer::tic()
 {
+	_seconds--;
+
+    if(_seconds < 0)
+    {
+        _seconds = 59;
+        _minutes--;
+    }
+
+    if(_minutes < 0)
+    {
+        _minutes = 59;
+        _hours--;
+    }
+
+    if(_hours < 0)
+    {
+        _hours = 23;
+        throw std::runtime_error("Timer expired");
+    }
 
 }
