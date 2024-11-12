0a3b37693a69c4df50abb412bf7c6a6f716b9008
Gabriela Cazares <cazgab67@gmail.com>
2024-11-12 12:49:26 -0600

    writing date.h based on the diagram provided

--- P11/full_credit/date.h
+++ P11/full_credit/date.h
@@ -3,8 +3,17 @@
 
 #include
 
+class Date
+{
+    private:
+        int _year;
+        int _month;
+        int _day;
 
-
-
+    public:
+        Date(int year, int month, int day) = default;
+        const operator<=>(const Date& date) = default;
+        friend std::ostream& operator<<(std::ostream& ost, const Date& date);
+};
 
 #endif
